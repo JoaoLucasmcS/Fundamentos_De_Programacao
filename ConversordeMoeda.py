92982dedf1a247a6c8a31e48f70adad6a13e0269
João Lucas <163215635+JoaoLucasmcS@users.noreply.github.com>
2024-04-04 20:40:36 -0300

    Conversor de Moedas
    O programa converte um valor em doláres para real

---
+++ ConversordeMoeda.py
@@ -0,0 +1,6 @@
+# Esse programa lê uma quantia em doláres e transforma ela em real
+
+valorD = float(input("Digite o valor em dólares: "))
+
+valorR = valorD/5.06
+print(f"O valor convertido é: R$ {valorR:.2f}")
