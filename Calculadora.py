3fe545cda5a37363351132e9f75e6bf87d461617
João Lucas <163215635+JoaoLucasmcS@users.noreply.github.com>
2024-04-04 20:00:26 -0300

    Calculadora 4 operações (py)
    O programa mostra a soma, subtração, multiplicação e divisão de dois números dados pelo usuário

---
+++ Calculadora.py
@@ -0,0 +1,9 @@
+# O programa mostra a soma,subtração, multiplicação e divisão de dois números dados pelo usuário
+
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo número: "))
+
+print("\nSoma:", num1, "+", num2, "=", num1+num2)
+print("Subtração:", num1, "-", num2, "=", num1-num2)
+print("Multiplicação:", num1, "x", num2, "=", num1*num2)
+print("Divisão:", num1, "/", num2, "=", num1/num2)
