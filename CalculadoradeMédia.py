96f7bd381ad99c57a4c2bc737e95cf7c244912c4
João Lucas <163215635+JoaoLucasmcS@users.noreply.github.com>
2024-04-04 20:47:13 -0300

    Calculadora de Média
    Esse programa calcula a média aritmética de três notas informadas pelo usuário

---
+++ CalculadoradeMédia.py
@@ -0,0 +1,8 @@
+#Esse programa calcula a média aritmética de três notas informadas pelo usuário
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+media = (nota1+nota2+nota3)/3
+
+print(f"A média das notas é: {media:.2f}")
